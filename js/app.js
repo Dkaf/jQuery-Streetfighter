1d6c4493e5e9f486bc8a5990ad45efb4dc7e4c6c
Dkaf <Daniel.j.kafer@gmail.com>
2016-08-08 16:58:07 -0500

    Finised jQuery street fighter page
    added pose animation and text

--- js/app.js
+++ js/app.js
@@ -2,10 +2,12 @@ $(document).ready(function(){
   $('.ryu').mouseenter(function() {
     $('.ryu-still').hide();
     $('.ryu-ready').show();
+    ryuReady +=  1;
   })
   .mouseleave(function() {
     $('.ryu-ready').hide();
     $('.ryu-still').show();
+    ryuReady -= 1;
   })
   .mousedown(function() {
     playHadouken();
@@ -24,7 +26,30 @@ $(document).ready(function(){
     $('.ryu-throwing').hide();
     $('.ryu-ready').show();
   });
+  $('body').keydown(function(e) {
+    if (e.which == 88, ryuReady == 0) {
+      $('.ryu-still').hide();
+      $('.ryu-cool').show();
+    }
+    else if (e.which == 88, ryuReady == 1 ) {
+      $('.ryu-ready').hide();
+      $('.ryu-throwing').hide();
+      $('.ryu-cool').show();
+    }
+  })
+  .keyup(function(e){
+    if (e.which == 88, ryuReady == 0) {
+      $('.ryu-cool').hide();
+      $('.ryu-still').show();
+    }
+    else if (e.which == 88, ryuReady == 1) {
+      $('.ryu-cool').hide();
+      $('.ryu-ready').show();
+    }
+  });
 });
+
+var ryuReady = 0;
 
 function playHadouken () {
   $('#hadouken-sound')[0].volume = 0.5;
